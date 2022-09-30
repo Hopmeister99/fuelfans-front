52e6f8ff4f7bd7768706605abd3155bc0e5bff4e
Stef Baeyens <stef.baeyens@axxes.com>
2022-09-30 12:00:47 +0200

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponentComponent } from './main-component/main-component.component';
+import { MapComponentComponent } from './map-component/map-component.component';
+import { ResultsComponentComponent } from './results-component/results-component.component';
+import { InputComponentComponent } from './input-component/input-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponentComponent,
+    MapComponentComponent,
+    ResultsComponentComponent,
+    InputComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/results-component/results-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-results-component',
+  templateUrl: './results-component.component.html',
+  styleUrls: ['./results-component.component.scss']
+})
+export class ResultsComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
